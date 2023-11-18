4ec5c9a39f340ab26df25674067d51f7ac15950c
Anikardava <anikardava97@gmail.com>
2023-11-18 02:46:11 -0800

    edit

--- 25 - Arcitectural Patterns/MainPage/View/MainPageView.swift
+++ 25 - Arcitectural Patterns/MainPage/View/MainPageView.swift
@@ -7,11 +7,47 @@
 
 import UIKit
 
+class MoviesCollectionView: UIView {
+    let collectionView: UICollectionView
+    
+    override init(frame: CGRect) {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        layout.minimumInteritemSpacing = 16
+        layout.minimumLineSpacing = 16
+        layout.sectionInset = UIEdgeInsets(top: 16, left: 16, bottom: 16, right: 16)
+        
+        self.collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        super.init(frame: frame)
+        
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        collectionView.backgroundColor = .clear
+        
+        setupCollectionView()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupCollectionView() {
+        addSubview(collectionView)
+        
+        NSLayoutConstraint.activate([
+            collectionView.topAnchor.constraint(equalTo: topAnchor),
+            collectionView.bottomAnchor.constraint(equalTo: bottomAnchor),
+            collectionView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            collectionView.trailingAnchor.constraint(equalTo: trailingAnchor)
+        ])
+    }
+}
+
 class MoviesListViewController: UIViewController {
     
     // MARK: - Properties
     
     private var viewModel = MoviesViewModel()
+    private var moviesCollectionView: MoviesCollectionView!
     
     private let logoImageView: UIImageView = {
         let imageView = UIImageView()
@@ -35,23 +71,11 @@ class MoviesListViewController: UIViewController {
         return button
     }()
     
-    private let collectionView: UICollectionView = {
-        let layout = UICollectionViewFlowLayout()
-        layout.scrollDirection = .vertical
-        layout.minimumInteritemSpacing = 16
-        layout.minimumLineSpacing = 16
-        layout.sectionInset = UIEdgeInsets(top: 16, left: 16, bottom: 16, right: 16)
-        
-        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
-        collectionView.translatesAutoresizingMaskIntoConstraints = false
-        collectionView.backgroundColor = .clear
-        return collectionView
-    }()
-    
     // MARK: - ViewLifeCycle
     override func viewDidLoad() {
         super.viewDidLoad()
         setup()
+        setupMoviesCollectionView()
         fetchMovies()
     }
     
@@ -59,7 +83,6 @@ class MoviesListViewController: UIViewController {
     private func setup() {
         view.backgroundColor = UIColor(red: 26/255.0, green: 34/255.0, blue: 50/255.0, alpha: 1)
         setupNavigationBar()
-        setupCollectionView()
     }
     
     private func setupNavigationBar() {
@@ -70,26 +93,20 @@ class MoviesListViewController: UIViewController {
         navigationItem.rightBarButtonItem = profileButtonItem
     }
     
-    private func setupCollectionView() {
-        view.addSubview(collectionView)
+    private func setupMoviesCollectionView() {
+        moviesCollectionView = MoviesCollectionView(frame: view.bounds)
+        moviesCollectionView.collectionView.dataSource = self
+        moviesCollectionView.collectionView.delegate = self
+        moviesCollectionView.collectionView.register(MovieItemCollectionViewCell.self, forCellWithReuseIdentifier: "MovieItemCell")
         
-        NSLayoutConstraint.activate([
-            collectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
-            collectionView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
-            collectionView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
-            collectionView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor)
-        ])
-        
-        collectionView.register(MovieItemCollectionViewCell.self, forCellWithReuseIdentifier: "MovieItemCell")
-        collectionView.dataSource = self
-        collectionView.delegate = self
+        view.addSubview(moviesCollectionView)
     }
     
     private func fetchMovies() {
         viewModel.fetchMovies { [weak self] success in
             if success {
                 DispatchQueue.main.async {
-                    self?.collectionView.reloadData()
+                    self?.moviesCollectionView.collectionView.reloadData()
                 }
             } else {
                 return
